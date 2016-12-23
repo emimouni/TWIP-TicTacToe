5f24af47227a21bef45aec842afa71331f530de9
emimouni <emimouni10@hotmail.com>
2016-12-23 10:23:14 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Would you like to play against the computer?");
+    }
+}
